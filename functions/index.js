16f3ab2bdd00176d95eb776c0d48a50b07192da3
Jake Arthur <jarthur.jmj@gmail.com>
2020-10-09 13:04:45 -0400

    setup complete

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,20 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+
+app.get("/auth", (request, response) => {
+    response.send("Auth called");
+});
+
+app.get('/data', (request, response) => {
+    response.send('Database call')
+})
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.app = functions.https.onRequest(app);
